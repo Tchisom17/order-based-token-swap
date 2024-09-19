65e180527e11c0006c041a09759b80d6dcc9decb
Tchisom <Kamsiyochukwu1@gmail..com>
2024-09-19 18:14:47 +0100

    Refactor OrderSwap contract for cleaner code and gas optimization

--- contracts/OrderSwap.sol
+++ contracts/OrderSwap.sol
@@ -7,6 +7,7 @@ contract OrderSwap {
 
     address public owner;
     uint256 public feePercentage;
+    uint256 public constant BASIS_POINTS = 10000;
 
     struct Order {
         address depositor;
@@ -38,8 +39,17 @@ contract OrderSwap {
         uint256 orderId
     );
 
+    error AmountMustBeGreaterThanZero();
+    error InvalidFeePercentage();
+    error InvalidSlippageTolerance();
+    error InsufficientBalanceOfDesiredToken();
+    error TransferFailed();
+    error OnlyDepositorCanCancel();
+    error OnlyOwnerCanSetFee();
+    error OrderNotActive();
+
     constructor(uint256 _feePercentage) {
-        require(_feePercentage <= 10000, "Fee percentage too high");
+        _validateFeePercentage(_feePercentage);
         owner = msg.sender;
         feePercentage = _feePercentage;
     }
@@ -51,10 +61,10 @@ contract OrderSwap {
         uint256 _amountDesired,
         uint256 _slippageTolerance
     ) external {
-        require(_amountDeposited > 0, "Amount deposited must be greater than 0");
-        require(_amountDesired > 0, "Amount desired must be greater than 0");
-        require(_slippageTolerance <= 10000, "Slippage tolerance too high");
-        require(IERC20(_tokenDeposited).transferFrom(msg.sender, address(this), _amountDeposited), "Transfer failed");
+        _validateAmount(_amountDeposited);
+        _validateAmount(_amountDesired);
+        _validateSlippageTolerance(_slippageTolerance);
+        _validateTransfer(IERC20(_tokenDeposited).transferFrom(msg.sender, address(this), _amountDeposited));
 
         orderCount++;
         orders[orderCount] = Order({
@@ -72,35 +82,66 @@ contract OrderSwap {
 
     function fulfillOrder(uint256 _id) external {
         Order storage order = orders[_id];
-        require(order.isActive, "Order is not active");
+        _validateOrderIsActive(order.isActive);
 
-        uint256 fee = (order.amountDesired * feePercentage) / 10000;
-        uint256 minAmountDesired = (order.amountDesired * (10000 - order.slippageTolerance)) / 10000;
-        // require(order.amountDesired >= minAmountDesired, "Amount does not meet slippage tolerance");
-        require(IERC20(order.tokenDesired).balanceOf(msg.sender) >= minAmountDesired, "Insufficient balance of desired token");
+        uint256 fee = (order.amountDesired * feePercentage) / BASIS_POINTS;
+        uint256 minAmountDesired = (order.amountDesired * (BASIS_POINTS- order.slippageTolerance)) / BASIS_POINTS;
+        _validateSufficientBalance(IERC20(order.tokenDesired).balanceOf(msg.sender), minAmountDesired);
 
         order.isActive = false;
-        require(IERC20(order.tokenDesired).transferFrom(msg.sender, order.depositor, order.amountDesired - fee), "Transfer failed");
-        require(IERC20(order.tokenDeposited).transfer(msg.sender, order.amountDeposited), "Transfer failed");
-        require(IERC20(order.tokenDesired).transfer(owner, fee), "Fee transfer failed");
+        _validateTransfer(IERC20(order.tokenDesired).transferFrom(msg.sender, order.depositor, order.amountDesired - fee));
+        _validateTransfer(IERC20(order.tokenDeposited).transfer(msg.sender, order.amountDeposited));
+        _validateTransfer(IERC20(order.tokenDesired).transfer(owner, fee));
 
         emit OrderFulfilled(_id, msg.sender);
     }
 
     function cancelOrder(uint256 _orderId) external {
         Order storage order = orders[_orderId];
-        require(order.depositor == msg.sender, "Only depositor can cancel the order");
-        require(order.isActive, "Order is not active");
+        _validateOnlyDepositor(msg.sender, order.depositor);
+        _validateOrderIsActive(order.isActive);
 
         order.isActive = false;
-        require(IERC20(order.tokenDeposited).transfer(msg.sender, order.amountDeposited), "Transfer failed");
+        _validateTransfer(IERC20(order.tokenDeposited).transfer(msg.sender, order.amountDeposited));
 
         emit OrderCancelled(_orderId);
     }
 
     function setFeePercentage(uint256 _feePercentage) external {
-        require(msg.sender == owner, "Only owner can set fee");
-        require(_feePercentage <= 10000, "Fee percentage too high");
+        _validateOnlyOwner(msg.sender);
+        _validateFeePercentage(_feePercentage);
         feePercentage = _feePercentage;
+    }
+
+    function _validateTransfer(bool success) internal pure {
+        if (!success) revert TransferFailed();
+    }
+
+    function _validateAmount(uint256 amount) internal pure {
+        if (amount == 0) revert AmountMustBeGreaterThanZero();
+    }
+
+    function _validateFeePercentage(uint256 _feePercentage) internal pure {
+        if (_feePercentage > 10000) revert InvalidFeePercentage();
+    }
+
+    function _validateSlippageTolerance(uint256 _slippageTolerance) internal pure {
+        if (_slippageTolerance > 10000) revert InvalidSlippageTolerance();
+    }
+
+    function _validateSufficientBalance(uint256 balance, uint256 minAmountDesired) internal pure {
+        if (balance < minAmountDesired) revert InsufficientBalanceOfDesiredToken();
+    }
+
+    function _validateOnlyDepositor(address sender, address depositor) internal pure {
+        if (sender != depositor) revert OnlyDepositorCanCancel();
+    }
+
+    function _validateOrderIsActive(bool isActive) internal pure {
+        if (!isActive) revert OrderNotActive();
+    }
+
+    function _validateOnlyOwner(address sender) internal view {
+        if (sender != owner) revert OnlyOwnerCanSetFee();
     }
 }
