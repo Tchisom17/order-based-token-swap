38058a6986d851f475654bd7d5431ac8813aa094
Tchisom <Kamsiyochukwu1@gmail.com>
2024-10-02 23:33:04 +0100

    feat: Add test for OrderSwap

--- test/OrderSwap.ts
+++ test/OrderSwap.ts
@@ -22,7 +22,7 @@ describe("OrderSwap", function () {
 
     const name2 = "Web3Bridge";
     const symbol2 = "W3B";
-    const totalSupply2 = ethers.parseUnits("1000");
+    const totalSupply2 = ethers.parseUnits("1000", 18);
 
     // const Token = await hre.ethers.getContractFactory("MockERC20");
     const tokenB = await Token.deploy(name2, symbol2, totalSupply2);
@@ -110,7 +110,7 @@ describe("OrderSwap", function () {
 
         const initialDepositorBalance = await tokenA.balanceOf(signer1);
         
-        expect(initialDepositorBalance).to.equal(amountDeposited);
+        expect(await tokenA.balanceOf(signer1)).to.equal(trfAmount);
 
         await orderSwap.connect(signer1).createOrder(tokenA, amountDeposited, tokenB, amountDesired, slippageTolerance);
 
@@ -120,5 +120,109 @@ describe("OrderSwap", function () {
         expect(finalDepositorBalance).to.equal(0);
         expect(finalContractBalance).to.equal(amountDeposited);
     });
+
+    it("Should emit event upon successful order creation", async function () {
+        const { orderSwap, owner, signer1, tokenA, tokenB } = await loadFixture(deployOrderSwap);
+    
+        await tokenA.transfer(signer1, trfAmount);
+        expect(await tokenA.balanceOf(signer1)).to.equal(trfAmount);
+
+        await tokenA.connect(signer1).approve(orderSwap, trfAmount);
+
+        // await orderSwap.connect(signer1).createOrder(tokenA, amountDeposited, tokenB, amountDesired, slippageTolerance);
+
+        expect( await orderSwap.connect(signer1).createOrder(tokenA, amountDeposited, tokenB, amountDesired, slippageTolerance))
+        .to.emit(orderSwap, "OrderCreated")
+        .withArgs(orderSwap.orderCount(), signer1.address, tokenA, amountDeposited, tokenB, amountDesired, slippageTolerance);
+    });
   });
+
+  describe("Confirm Order", function () {
+    it("Should fail to confirm if order is not active ", async function () {
+      const { orderSwap, owner, signer1, signer2, tokenA, tokenB } = await loadFixture(deployOrderSwap);
+      // const depAmount = ethers.parseUnits("10", 18)
+    //   const amountDeposit = ethers.parseUnits("0", 18);
+    const orderSwapBalance = await tokenB.balanceOf(orderSwap);
+
+      await tokenA.transfer(signer1, trfAmount);
+      await tokenA.connect(signer1).approve(orderSwap, trfAmount);
+      await orderSwap.connect(signer1).createOrder(tokenA, amountDeposited, tokenB, amountDesired, slippageTolerance);
+
+      await tokenB.transfer(signer2, trfAmount);
+      await tokenB.connect(signer2).approve(orderSwap, trfAmount);
+
+      const id = 1;
+
+      await orderSwap.connect(signer2).confirmOrder(id);
+      // await expect(orderSwap.connect(signer2).confirmOrder(id))
+      //       .to.emit(orderSwap, "OrderFulfilled")
+      //       .withArgs(id, await signer2.getAddress());
+      //       const order = await orderSwap.orders(id);
+      //   expect(order.isActive).to.be.false;
+    });
+
+    // it("Should fail if amount desired equals zero", async function () {
+    //     const { orderSwap, signer1, tokenA, tokenB } = await loadFixture(deployOrderSwap);
+  
+    //     const amountDesire = ethers.parseUnits("0", 18);
+  
+    //     await tokenA.transfer(signer1, trfAmount);
+    //     await tokenA.connect(signer1).approve(orderSwap, trfAmount);
+    //     await expect(orderSwap.connect(signer1).createOrder(tokenA, amountDeposited, tokenB, amountDesire, slippageTolerance)).to.be.revertedWithCustomError(orderSwap, "AmountMustBeGreaterThanZero");
+    //   });
+
+    // it("Should fail if slippage is greater than 10000", async function () {
+    //     const { orderSwap, signer1, tokenA, tokenB } = await loadFixture(deployOrderSwap);
+    //     const slippage = 11000;
+
+    //     await tokenA.transfer(signer1, trfAmount);
+    //     await tokenA.connect(signer1).approve(orderSwap, trfAmount);
+    //     await expect(orderSwap.connect(signer1).createOrder(tokenA, amountDeposited, tokenB, amountDesired, slippage)).to.be.revertedWithCustomError(orderSwap, "InvalidSlippageTolerance");
+    // });
+
+    // // it("Should fail if transfer to contract is not successful", async function () {
+    // //     const { orderSwap, signer1, tokenA, tokenB } = await loadFixture(deployOrderSwap);
+
+    // //     await tokenA.transfer(signer1, trfAmount);
+    // //     await tokenA.connect(signer1).approve(orderSwap, amountDeposited);
+    // //     await expect(orderSwap.connect(signer1).createOrder(tokenA, amountDeposited, tokenB, amountDesired, slippageTolerance)).to.be.revertedWithCustomError(orderSwap, "TransferFailed");
+    // // });
+
+    // it("Should create order successfully", async function () {
+    //     const { orderSwap, owner, signer1, tokenA, tokenB } = await loadFixture(deployOrderSwap);
+    
+    //     await tokenA.transfer(signer1, trfAmount);
+    //     expect(await tokenA.balanceOf(signer1)).to.equal(trfAmount);
+
+    //     await tokenA.connect(signer1).approve(orderSwap, trfAmount);
+
+    //     const initialDepositorBalance = await tokenA.balanceOf(signer1);
+        
+    //     expect(initialDepositorBalance).to.equal(amountDeposited);
+
+    //     await orderSwap.connect(signer1).createOrder(tokenA, amountDeposited, tokenB, amountDesired, slippageTolerance);
+
+    //     const finalDepositorBalance = await tokenA.balanceOf(signer1);
+    //     const finalContractBalance = await tokenA.balanceOf(orderSwap);
+
+    //     expect(finalDepositorBalance).to.equal(0);
+    //     expect(finalContractBalance).to.equal(amountDeposited);
+    // });
+
+    // it("Should emit event upon successful order creation", async function () {
+    //     const { orderSwap, owner, signer1, tokenA, tokenB } = await loadFixture(deployOrderSwap);
+    
+    //     await tokenA.transfer(signer1, trfAmount);
+    //     expect(await tokenA.balanceOf(signer1)).to.equal(trfAmount);
+
+    //     await tokenA.connect(signer1).approve(orderSwap, trfAmount);
+
+    //     // await orderSwap.connect(signer1).createOrder(tokenA, amountDeposited, tokenB, amountDesired, slippageTolerance);
+
+    //     await expect(orderSwap.connect(signer1).createOrder(tokenA, amountDeposited, tokenB, amountDesired, slippageTolerance))
+    //     .to.emit(orderSwap, "OrderCreated")
+    //     .withArgs(1, signer1.address, tokenA, amountDeposited, tokenB, amountDesired, slippageTolerance);
+    // });
+  });
+
 });
